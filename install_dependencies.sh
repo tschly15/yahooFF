652bfcd168c06066b0653a683284c43d828b4ab0
tschly15 <cpuengineer5@yahoo.com>
2020-01-09 16:37:10 -0600

    update the dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,3 +6,5 @@ pip install setuptools --upgrade
 pip install cffi --upgrade
 pip install enum34 ipaddress
 pip install flask requests pyopenssl cryptography oauthlib dpath
+pip install flask_login
+pip install pygresql
