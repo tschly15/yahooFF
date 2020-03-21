a14ba3877c073659676059845331e6f6fdd43dcf
tschly15 <cpuengineer5@yahoo.com>
2020-03-21 06:45:48 -0500

    additional dependencies; include ignore

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+virtualenv flask
 cd flask
-source bin/activate
+source bin/activate || exit
 
 pip install setuptools --upgrade
 pip install cffi --upgrade
 pip install enum34 ipaddress
 pip install flask requests pyopenssl cryptography oauthlib dpath
-pip install flask_login
 pip install pygresql
+pip install flask_login
 pip install flask-sqlalchemy
+pip install flask-migrate
